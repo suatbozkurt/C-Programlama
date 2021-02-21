625abea9b62d8f4987266dd4ca029c259ac6fed1
suatbozkurt <46561217+suatbozkurt@users.noreply.github.com>
2021-02-22 01:37:15 +0300

    Add files via upload

---
+++ 2.2 Sum2.c
@@ -0,0 +1,23 @@
+//C ile Programlama
+//Bolum 2
+
+//Suat Bozkurt
+//22.02.2021
+
+#include <stdio.h>
+
+int main(void)
+{
+	int integer1;
+	int integer2;
+	int sum;
+	
+	printf("Enter first integer\n");
+	scanf("%d",&integer1);
+	
+	printf("Enter second integer \n");
+	scanf("%d", &integer2);
+
+	printf("sum is %d\n",integer1 + integer2);
+	
+}
